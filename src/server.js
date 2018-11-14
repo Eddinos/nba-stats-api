6bdbbddad84366d2917cb8f77b7765fd1e342c97
Eddine Djerboua <eddine.djerboua@gmail.com>
2018-11-14 22:50:37 +0100

    new mlab server

--- src/server.js
+++ src/server.js
@@ -5,7 +5,7 @@ var app = express();
 import routes from './routes'
 import bodyParser from 'body-parser';
 const PORT = process.env.PORT || 3000;
-const DB_CONNECTION = process.env.NODE_ENV == 'production' ? `mongodb://${process.env['db-user-name']}:${process.env['db-user-lemonade']}@ds139278.mlab.com:39278/nba-stats` : `mongodb://localhost:27017/NBA-stats`
+const DB_CONNECTION = process.env.NODE_ENV == 'production' ? `mongodb://${process.env['db-user-name']}:${process.env['db-user-lemonade']}@ds123753.mlab.com:23753/nba-stats` : `mongodb://localhost:27017/NBA-stats`
 
 mongoose.connect(DB_CONNECTION);
 var db = mongoose.connection;
